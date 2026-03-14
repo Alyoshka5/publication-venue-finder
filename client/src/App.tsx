1b632dac8dae0fe8361d1fcedc06a0154d405306
Alyoshka <alyoshkamyshko@gmail.com>
2026-03-13 20:43:08 -0700

    feat: establish React app's connection with express server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,6 +6,16 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const getServerResponse = async () => {
+      const res = await fetch('http://localhost:3000/');
+      const data = await res.json();
+      console.log(data);
+    }
+
+    getServerResponse();
+  }, []);
 
   return (
     <>
