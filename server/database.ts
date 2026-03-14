e9b506ef2c9ae76caee09b57a2917f6e7bda81b4
Alyoshka <alyoshkamyshko@gmail.com>
2026-03-13 22:52:31 -0700

    feat: set up db configuration

---
+++ server/database.ts
@@ -0,0 +1,9 @@
+import mysql from 'mysql2';
+import 'dotenv/config';
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'publication_venue_finder_db'
+}).promise();
