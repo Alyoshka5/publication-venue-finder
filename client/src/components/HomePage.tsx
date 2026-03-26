0a1960fe1a04d3cf6034f8dce51155d8b9448316
Alyoshka <alyoshkamyshko@gmail.com>
2026-03-26 13:44:35 -0700

    refactor: reorganize code in seperate folders and files

---
+++ client/src/components/HomePage.tsx
@@ -0,0 +1,15 @@
+import VenueListHeader from './VenueListHeader'
+import VenueList from './VenueList'
+import { useState } from 'react'
+
+
+export default function HomePage() {
+  const [query, setQuery] = useState<string>('');
+
+  return (
+    <div className="page">
+      <VenueListHeader query={query} setQuery={setQuery}  />
+      <VenueList query={query} />
+    </div>
+  )
+}
---
+++ client/src/models/venues.ts
@@ -0,0 +1,12 @@
+type UpcomingVenue = {
+  seriesId: number
+  acronym: string | null
+  year: number
+  title: string
+  city: string
+  country: string
+  submissionDeadline: string | null
+  organization: string
+}
+
+export type { UpcomingVenue }
